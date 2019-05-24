362f2da51f3ff8b4b5d1ada3a2349d7ed7d2d5d7
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2019-05-24 08:38:02 +0500

    Add files via upload

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,1 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using CommonClasses;
+using CommonClasses.Console;
 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var res = DB.GetTable("c:\\Test\\Database1.mdb", "Table1");
+
+                Utils.PrintTable(res, Console.Out);                
+                
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Default.OnError(ex);
+            }
+            Console.ReadKey();
+        }
+    }
+}
